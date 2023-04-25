55fb3ab0286e1720a093bc709e5c07f7b2e97d04
ahmedosamaissa23 <131785490+ahmedosamaissa23@users.noreply.github.com>
2023-04-25 16:22:22 +0200

    Add files via upload

---
+++ osos.js
@@ -0,0 +1,10 @@
+function showLineup() {
+  const goalkeeper = "ثيبو كورتوا";
+  const defense = "داني كارفاخال، سيرجيو راموس، رافائيل فاران، فيرلاند ميندي";
+  const midfield = "كاسيميرو، لوكا مودريتش، توني كروس";
+  const attack = "كريم بنزيما، إيدين هازارد، رودريجو";
+  document.getElementById("goalkeeper").innerHTML = goalkeeper;
+  document.getElementById("defense").innerHTML = defense;
+  document.getElementById("midfield").innerHTML = midfield;
+  document.getElementById("attack").innerHTML = attack;
+}
